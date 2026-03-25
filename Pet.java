30e1e4ae698997aecbb92ad9867b79180822af53
Marcos Aleixo <maknytz@gmail.com>
2026-03-24 21:38:40 -0300

    Modificando o construtor da classe

--- Pet.java
+++ Pet.java
@@ -2,7 +2,7 @@ public class Pet {
     private final String name;
     private boolean clean;
 
-    public Pet(String name, boolean clean) {
+    public Pet(String name) {
         this.name = name;
         this.clean = false;
     }
